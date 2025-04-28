f73f43e001580fa009d55efec4bb6bc728f48112
Andrea-Sgura <andreasgura96@gmail.com>
2025-04-28 08:04:21 +0200

    [UPD] Inserite funzioni nel file postController

---
+++ controllers/postController.js
@@ -0,0 +1,31 @@
+const posts = require (`../data/posts.js`);
+
+// index
+function index (req, res){
+    res.send(`Elenco dei post`);
+}
+
+// show
+function show (req, res) {
+    res.send(`Post con id: ${req.params.id}`);
+}
+
+// store
+function store (req, res) {
+    res.send(`Inserimento di un nuovo post`);
+}
+
+// update
+function update (req, res) {
+    res.send (`Modifica totale del post con id: ${req.params.id}`);
+}
+
+// modify
+function modify (req, res) {
+    res.send (`Modifica parziale del post con id: ${req.params.id}`);
+}
+
+// destroy
+function destroy (req, res) {
+    res.send(`Cancellazione del post con id: ${req.params.id}`);
+}
