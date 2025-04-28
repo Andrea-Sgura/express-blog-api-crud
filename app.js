803925b6094d7604ee86ee024cf3c8cd44088fd4
Andrea-Sgura <andreasgura96@gmail.com>
2025-04-28 06:26:05 +0200

    [UPD] Importato express in app.js, definita porta e messa in ascolto

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-console.log(`Hello World`);
+// importo express
+const express = require(`express`);
+
+// inizializzo express nella variabile app
+const app = express();
+
+// definiamo la porta
+const port = 3000;
+
+app.get(`/`, (req, res) => {
+    console.log(`Hello world`);
+})
+
+app.listen(port, () => {
+    console.log(`Server in ascolto nella porta ${port}`);
+})
