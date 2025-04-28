e4d1d4e8c84a50d56e209102ee5d61cf299bc2a1
Andrea-Sgura <andreasgura96@gmail.com>
2025-04-28 07:27:37 +0200

    [UPD] Definite rotte per i post

--- router/post.js
+++ router/post.js
@@ -3,3 +3,35 @@ const express = require(`express`);
 
 // importo la classe router di express
 const router = express.Router();
+
+// Definizione delle rotte per i post
+
+// index
+router.get(`/`, (req, res) => {
+    res.send(`Elenco dei post`);
+})
+
+// show
+router.get(`/:id`, (req, res) => {
+    res.send(`Post con id: ${req.params.id}`);
+})
+
+// store
+router.post(`/`, (req, res) => {
+    res.send(`Inserimento di un nuovo post`);
+})
+
+// update
+router.put(`/:id`, (req, res) =>{
+    res.send(`Modifica totale del post con id: ${req.params.id}`);
+})
+
+// modify
+router.patch(`/:id`, (req, res) => {
+    res.send(`Modifica parziale del post con id: ${req.params.id}`);
+})
+
+// destroy
+router.delete(`/:id`, (req, res) => {
+    res.send (`Cancellazione del post con id: ${req.params.id}`);
+})
